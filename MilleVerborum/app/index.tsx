a89bc39113363a14e4fc238d83c7ba79291738c7
Paul Gunnarsson <paulgun2013@gmail.com>
2025-07-11 17:07:05 +0100

    Created a pre-populated database, with some data already loaded. The app now takes that DB and loads it into its own SQLite folder, which it can then query.

--- MilleVerborum/app/index.tsx
+++ MilleVerborum/app/index.tsx
@@ -1,24 +1,46 @@
+import { openLanguageDatabase } from '@/db/openDatabase';
+
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link } from 'expo-router';
+import React, { useEffect, useState } from 'react';
 import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View style={styles.container}>
-        <Link href="/LanguageScreen" asChild>
-            <Pressable style={styles.button}>
-                <FontAwesome name="arrow-right" size={30} color="#25292e" style={styles.buttonIcon} />
-                <Text style={styles.buttonLabel}>Start</Text>
-            </Pressable>
-        </Link>
-        <Link href="/" asChild>
-            <Pressable style={styles.button}>
-                <FontAwesome name="bar-chart" size={30} color="#25292e" style={styles.buttonIcon} />
-                <Text style={styles.buttonLabel}>Stats</Text>
-            </Pressable>
-        </Link>
-    </View>
-  );
+
+    const [items, setItems] = useState<any[]>([]);
+
+    useEffect(() => {
+        // console.log('running useEffect, about to call db');
+        (async () => {
+            try{
+                // console.log('sending async request...');
+                const db = await openLanguageDatabase();
+                // console.log('opened db, preparing query for languages...');
+
+                const result = await db.getAllAsync("SELECT * FROM languages;");
+                console.log('Result:', result); // Should have .rows._array
+            } catch (error) {
+                console.error("DB failed to open", error);
+            }
+        })();
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <Link href="/LanguageScreen" asChild>
+                <Pressable style={styles.button}>
+                    <FontAwesome name="arrow-right" size={30} color="#25292e" style={styles.buttonIcon} />
+                    <Text style={styles.buttonLabel}>Start</Text>
+                </Pressable>
+            </Link>
+            <Link href="/" asChild>
+                <Pressable style={styles.button}>
+                    <FontAwesome name="bar-chart" size={30} color="#25292e" style={styles.buttonIcon} />
+                    <Text style={styles.buttonLabel}>Stats</Text>
+                </Pressable>
+            </Link>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
---
+++ MilleVerborum/assets/database/pre_pop_database.sql
@@ -0,0 +1,77 @@
+DROP TABLE IF EXISTS languages;
+DROP TABLE IF EXISTS words;
+DROP TABLE IF EXISTS test;
+
+PRAGMA foreign_keys = ON;
+
+
+CREATE TABLE IF NOT EXISTS languages (
+    lang_id         INTEGER     PRIMARY KEY,
+    lang_name       TEXT        NOT NULL UNIQUE,
+    lang_abv        TEXT        NOT NULL UNIQUE,
+    curr_level      INTEGER,
+    prime_col       TEXT,
+    sec_col         TEXT,
+    ter_col         TEXT
+);
+
+CREATE TABLE IF NOT EXISTS words (
+    word_id         INTEGER     PRIMARY KEY,
+    lang_id         INTEGER     NOT NULL,
+    word_rank       INTEGER     NOT NULL,
+    native_word     TEXT        NOT NULL,
+    foreign_word    TEXT        NOT NULL,
+    pronunciation   TEXT        NOT NULL,
+    corr_count      INTEGER     NOT NULL,
+    fail_count      INTEGER     NOT NULL,
+    FOREIGN KEY (lang_id) REFERENCES languages (lang_id),
+    UNIQUE (lang_id, word_rank)
+);
+
+INSERT INTO languages (
+    lang_name,
+    lang_abv,
+    curr_level,
+    prime_col,
+    sec_col,
+    ter_col
+) values (
+    "Danish",
+    "DA",
+    null,
+    '#C8102E',
+    '#FFFFFF',
+    null
+);
+
+INSERT INTO languages (
+    lang_name,
+    lang_abv,
+    curr_level,
+    prime_col,
+    sec_col,
+    ter_col
+) values (
+    "Estonian",
+    "ET",
+    null,
+    '#0072CE',
+    '#000000',
+    '#FFFFFF'
+);
+
+INSERT INTO languages (
+    lang_name,
+    lang_abv,
+    curr_level,
+    prime_col,
+    sec_col,
+    ter_col
+) values (
+    "Lithuanian",
+    "LT",
+    null,
+    '#FFB81C',
+    '#046A38',
+    '#BE3A34'
+);
