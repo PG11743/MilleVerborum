c817c6cafe83c5c088d51973878b6c5a6a452479
Paul Gunnarsson <paulgunnarsson98@gmail.com>
2025-10-05 15:17:06 +0000

    Upgraded the local database and filesystem interactions, to bring it in line with the new system in Expo SDK 54.

--- MilleVerborum/db/openDatabase.ts
+++ MilleVerborum/db/openDatabase.ts
@@ -1,6 +1,5 @@
-// openDatabase.ts
 import { Asset } from 'expo-asset';
-import * as FileSystem from 'expo-file-system/legacy';
+import { File, Directory, Paths } from 'expo-file-system';
 import { openDatabaseAsync, SQLiteDatabase } from 'expo-sqlite';
 
 const DB_NAME = 'milleverborum.db';
@@ -8,28 +7,48 @@ const DB_NAME = 'milleverborum.db';
 // A temp solution for resetting the database. Something more clean will be needed eventually
 const wipeFlag = false;
 
+async function loadPrebuiltDatabase(sqliteFolder: Directory) {
+    const asset = Asset.fromModule(require('@/assets/database/milleverborum.db'));
+
+    sqliteFolder.create();
+
+    await asset.downloadAsync();
+
+    const fileAsset = new File (asset.localUri ?? "");
+    const fileDest = new File (sqliteFolder, asset.name + '.' + asset.type);
+
+    fileAsset.copy(fileDest);
+    console.log(console.log('listing contents of the sqlite folder: ', sqliteFolder.list()));
+
+}
+
 export async function openLanguageDatabase(): Promise<SQLiteDatabase> {
-    // console.log('entered async function openLanguageDatabase...');
-    const sqliteFolder = `${FileSystem.documentDirectory}SQLite`;
-    const dbPath = `${sqliteFolder}/${DB_NAME}`;
-    // console.log('dbPath is said to be: ', dbPath);
-    const fileInfo = await FileSystem.getInfoAsync(dbPath);
-    if (!fileInfo.exists || wipeFlag) {
-        const asset = Asset.fromModule(require('@/assets/database/milleverborum.db'));
+    const sqliteFolder = new Directory(Paths.document, 'SQLite');
+    let foundDBFlag: boolean = false;
 
-        await asset.downloadAsync();
-
-        // Ensure SQLite folder exists
-        await FileSystem.makeDirectoryAsync(sqliteFolder, { intermediates: true });
-
-        // Copy the downloaded DB to the SQLite folder
-        await FileSystem.copyAsync({
-            from: asset.localUri!,
-            to: dbPath,
-        });
+    if (wipeFlag) {
+        console.log('Wipe Flag activated, wiping DB');
+        sqliteFolder.delete();
+        await loadPrebuiltDatabase(sqliteFolder);
     }
-                
+
+    if (sqliteFolder.exists) {
+        for (const file of sqliteFolder.list()) {
+            
+            if (((new File(file.name)).name).includes('milleverborum.db')) {
+                foundDBFlag = true;
+            }
+        }
+    } else {
+        sqliteFolder.create();
+    }
+
+    if (!foundDBFlag) {
+        console.log('did not find local DB');
+        sqliteFolder.delete();
+        await loadPrebuiltDatabase(sqliteFolder);
+    }
+
 
     return await openDatabaseAsync(DB_NAME, {useNewConnection: true});
-    // return await openDatabaseAsync(dbPath.replace('file://', ''));
 }
