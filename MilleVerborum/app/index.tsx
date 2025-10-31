1e9d55a0b9b737cbc5bad0d31e127931dcff7701
Paul Gunnarsson <paulgun2013@gmail.com>
2025-10-31 18:37:18 +0000

    Changed the landing page to use a new font colour, based on the gradient.

--- MilleVerborum/app/index.tsx
+++ MilleVerborum/app/index.tsx
@@ -3,6 +3,7 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Link } from 'expo-router';
 import { Pressable, StatusBar, StyleSheet, Text, View } from "react-native";
+import MaskedView from '@react-native-masked-view/masked-view';
 
 export default function Index() {
     return (
@@ -11,19 +12,69 @@ export default function Index() {
                 colors={['#7700ffff', '#00f9ff']}
                 style={StyleSheet.absoluteFill}
             />
+            <StatusBar backgroundColor="#00f9ff" />
             <Link href="/LanguageScreen" asChild>
                 <Pressable style={styles.button}>
-                    <FontAwesome name="arrow-right" size={30} style={styles.buttonIcon} />
-                    <Text style={styles.buttonLabel}>Start</Text>
+                    <MaskedView
+                    style={{ flex: 1, flexDirection: 'row', height: '100%' }}
+                    maskElement={
+                        <View
+                            style={{
+                                // Transparent background because mask is based off alpha channel.
+                                backgroundColor: 'transparent',
+                                flex: 1,
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <View
+                                style={{flexDirection: 'row'}}
+                            >
+                                <FontAwesome name="arrow-right" size={30} style={styles.buttonIcon} />
+                                <Text style={styles.buttonLabel}>Start</Text>
+                            </View>
+                        </View>
+                    }
+                    >
+                        {/* Shows behind the mask, you can put anything here, such as an image */}
+                        <LinearGradient
+                            colors={['#7700ffff', '#00f9ff']}
+                            style={StyleSheet.absoluteFill}
+                        />
+                    </MaskedView>
                 </Pressable>
             </Link>
             <Link href="/" asChild>
                 <Pressable style={styles.button}>
-                    <FontAwesome name="bar-chart" size={30} style={styles.buttonIcon} />
-                    <Text style={styles.buttonLabel}>Stats</Text>
+                    <MaskedView
+                    style={{ flex: 1, flexDirection: 'row', height: '100%' }}
+                    maskElement={
+                        <View
+                            style={{
+                                // Transparent background because mask is based off alpha channel.
+                                backgroundColor: 'transparent',
+                                flex: 1,
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <View
+                                style={{flexDirection: 'row'}}
+                            >
+                                <FontAwesome name="bar-chart" size={30} style={styles.buttonIcon} />
+                                <Text style={styles.buttonLabel}>Stats</Text>
+                            </View>
+                        </View>
+                    }
+                    >
+                        {/* Shows behind the mask, you can put anything here, such as an image */}
+                        <LinearGradient
+                            colors={['#7700ffff', '#00f9ff']}
+                            style={StyleSheet.absoluteFill}
+                        />
+                    </MaskedView>
                 </Pressable>
             </Link>
-            <StatusBar backgroundColor="#00f9ff" />
         </View>
     );
 }
@@ -44,6 +95,8 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         flexDirection: 'row',
+        minHeight: 75,
+        maxWidth: 200
     },
     buttonLabel: {
         fontSize:       30,
