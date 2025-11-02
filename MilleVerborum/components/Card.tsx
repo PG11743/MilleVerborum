017f125885a3d5d8263c65f5fb90ad1731445e01
Paul Gunnarsson <paulgun2013@gmail.com>
2025-11-02 15:59:32 +0000

    Changed the appearance of the card, removing the variation from the language, and adding gradient colour.

--- MilleVerborum/components/Card.tsx
+++ MilleVerborum/components/Card.tsx
@@ -1,4 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import MaskedView from '@react-native-masked-view/masked-view';
 
 type Props = {
     nativeText :        string;
@@ -12,11 +14,47 @@ type Props = {
 export default function Card (props : Props) {
     // console.log(props);
     return (
-        <View style={[styles.renderCardContainer, {backgroundColor: props.backgroundColour}]}>
-            <View style={[styles.innerCardContainer, {backgroundColor: props.backgroundColour, borderColor: props.borderColour}]}>
+        <View style={styles.renderCardContainer}>
+                <LinearGradient
+                    colors={['#00f9ff', '#7700ffff']}
+                    style={styles.linearGradientBorder}
+                >
+            <View style={styles.innerCardContainer}>
                 {props.foreignText ? (
                 <View style={styles.cardTextContainer}>
-                    <View>
+                                        <MaskedView
+                    style={{ flex: 1, flexDirection: 'row', height: '100%', width: '100%' }}
+                    maskElement={
+                        <View
+                            style={{
+                                // Transparent background because mask is based off alpha channel.
+                                backgroundColor: 'transparent',
+                                flex: 1,
+                                paddingTop: 200,
+                                alignItems: 'center',
+                            }}
+                        >
+                            <View>
+                                <View style={styles.nativeBox}>
+                                    <Text style={styles.text}>{props.nativeText}</Text>
+                                </View>
+                                <View style={styles.foreignBox}>
+                                    <Text style={styles.subtext}>{props.foreignText}</Text>
+                                </View>
+                                <View style={styles.pronunciationBox}>
+                                    <Text style={styles.subtext}>[{props.pronunciation}]</Text>
+                                </View>
+                            </View>
+                        </View>
+                    }
+                    >
+                        {/* Shows behind the mask, you can put anything here, such as an image */}
+                        <LinearGradient
+                            colors={['#00f9ff', '#7700ffff']}
+                            style={StyleSheet.absoluteFill}
+                        />
+                    </MaskedView>
+                    {/* <View>
                         <Text style={[styles.text, {color: props.textColour}]}>{props.nativeText}</Text>
                     </View>
                     <View style={[styles.foreignBox, {borderColor: props.borderColour}]}>
@@ -24,16 +62,40 @@ export default function Card (props : Props) {
                     </View>
                     <View>
                         <Text style={[styles.subtext, {color: props.textColour}]}>[{props.pronunciation}]</Text>
-                    </View>
+                    </View> */}
                 </View>
-                ) : (
+                ) : (                    
                 <View style={styles.cardTextContainer}>
-                    <View>
-                        <Text style={[styles.text, {color: props.textColour}]}>{props.nativeText}</Text>
-                    </View>
+                    <MaskedView
+                    style={{ flex: 1, flexDirection: 'row', height: '100%', width: '100%' }}
+                    maskElement={
+                        <View
+                            style={{
+                                // Transparent background because mask is based off alpha channel.
+                                backgroundColor: 'transparent',
+                                flex: 1,
+                                paddingTop: 200,
+                                alignItems: 'center',
+                            }}
+                        >
+                            <View
+                                style={{flexDirection: 'row'}}
+                            >
+                                <Text style={styles.text}>{props.nativeText}</Text>
+                            </View>
+                        </View>
+                    }
+                    >
+                        {/* Shows behind the mask, you can put anything here, such as an image */}
+                        <LinearGradient
+                            colors={['#00f9ff', '#7700ffff']}
+                            style={StyleSheet.absoluteFill}
+                        />
+                    </MaskedView>
                 </View>
                 )}
             </View>
+            </LinearGradient>
         </View>
     );
 }
@@ -41,28 +103,36 @@ export default function Card (props : Props) {
 const styles = StyleSheet.create({
     renderCardContainer: {
         flex: 1,
-        borderRadius: 15,
+        borderRadius: 35,
         alignItems: 'center',
         width: '100%',
         height: '100%',
-        padding: 30
+        padding: 30,
+        backgroundColor: '#ffffff'
+    },
+    linearGradientBorder: {
+        height: '100%',
+        width:  '100%',
+        borderRadius: 17,
+        padding: 3
     },
     innerCardContainer: {
-        paddingTop: 200,
         flex: 1,
         width: '100%',
         height: '100%',
-        borderRadius: 15,
-        borderWidth: 1
+        borderRadius: 14,
+        // paddingTop: 200,
+        backgroundColor: '#ffffff'
     },
     cardTextContainer: {
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        height: '100%',
+        width: '100%',
+        flex: 1
     },
     nativeBox:  {
-        borderBottomWidth: 2,
-        borderColor: 'black',
-        paddingBottom: 5
+        alignItems: 'center'
     },
     foreignBox: {
         alignItems: 'center',
@@ -70,13 +140,19 @@ const styles = StyleSheet.create({
         borderColor: 'black',
         paddingTop: 20,
         marginTop: 20,
+        marginLeft: 'auto',
+        marginRight: 'auto',
         width: 100,
         height: 100
     },
     text:   {
-        fontSize: 40
+        fontSize: 40,
+        color: 'black'
     },
     subtext:    {
         fontSize: 30
+    },
+    pronunciationBox: {
+        alignItems: 'center'
     }
 });
